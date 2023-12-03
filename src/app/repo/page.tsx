2735ee20f6ddaab28534ea4286d2a8f0c5cf9681
vane6892 <145076072+vane6892@users.noreply.github.com>
2023-12-03 15:42:19 -0400

    Revert "SEM-2"

--- src/app/repo/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Repo() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>REPOSITORIO</h1>
-      </main>
-    )
-  }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,30 +1,19 @@
-'use client'
-import styles from './styles.module.scss';
-import Image from 'next/image';
-
-
-import MenuIcon from '../MenuIcon/MenuHamburger'
-import MenuList from '../Menu/MenuList'
-import tupa from 'public/assets/img/icon-tupa.svg'
-import logo from 'public/assets/img/logo.png'
-
+import Link from 'next/link';
+import styles from './styles.module.css';
 const Navbar = () => {
     return (
-        <nav >
-            <div className={styles.nav} >
-                <div className={styles.rectangle1}> 
-                    <div className={styles.rectangle2}>
-                        <Image src={logo} alt="image logo"  />
-                    </div>
-                    
-                    <MenuIcon/>
-
-                    <button className={styles.icon}>
-                        <Image  src={tupa} alt="tupa icon" />
-                    </button>  
-                </div>
-            </div>
-            
+        <nav className={styles.nav}>
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/post">Posts</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+        </ul>
         </nav>
     );
 };
