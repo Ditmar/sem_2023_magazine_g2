e30059de4c89155f2d6d20ddea270d249db0ee30
JULIO CESAR FLORES RODRIGUEZ <juliobolivianojc17@gmail.com>
2023-12-04 12:59:07 -0400

    feature(common): add articles[SEM-4]

---
+++ src/app/PublicationArticles/NewArticulo/page.tsx
@@ -0,0 +1,11 @@
+import NewArticulo from "@/components/NewArticulo/NewArticulo"
+import styles from "@/components/PublicationArticles/NewArticulo/styles.module.css"
+const page = () => {
+  return (
+    <div>
+      <NewArticulo/>
+    </div>     
+  )
+}
+
+export default page
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import Article from "../components/PublicationArticles/page"
 
 const inter = Inter({ subsets: ['latin'] })
 
