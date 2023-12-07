c42ea3f651e16a9d188c79ede3d66d5dda8dfa4a
saddamlopez <145850996+saddamlopez@users.noreply.github.com>
2023-12-06 20:38:35 -0400

    Revert "Revert "features (commom):added component Volumenes [SEM-10]""

--- src/components/MainNavigation/MainNavigation.tsx
+++ src/components/MainNavigation/MainNavigation.tsx
@@ -1,10 +1,23 @@
+// MainNavigation.js
+import React from 'react';
+import Link from 'next/link';
 import styles from './styles.module.css';
-const MainNavigation = () => {
 
-    return (
-        <nav className={styles.nav}>
-            Menu here
-        </nav>
-    )
-}
+const MainNavigation = () => {
+  const handleListarVolumenes = () => {
+    // Lógica para listar volúmenes
+    console.log('Lista de volúmenes');
+    // Aquí podrías hacer una llamada a una API, acceder a un estado de la aplicación, etc.
+  };
+
+  return (
+    <nav className={styles.nav}>
+     
+      <Link href="/">Inicio</Link>
+      {/* Otros enlaces del menú */}
+    </nav>
+  );
+};
+
 export default MainNavigation;
+
