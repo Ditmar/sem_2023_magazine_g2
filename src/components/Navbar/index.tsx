f14f784ad717750bc8ba7b68a4e7998de8356cbc
jesusdx9 <jesusdx9@gmail.com>
2023-12-10 09:13:04 -0400

    feature(common):first pr

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,71 +1,20 @@
-// @ts-nocheck
-'use client';
-import { useState } from 'react';
-import styles from './styles.module.scss';
 import Link from 'next/link';
-import Years from '../../app/years/page';
-
-const Navbar: React.FC = () => {
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [showYears, setShowYears] = useState(false);
-
-  const handleItemClick = (itemName: string) => {
-    if (itemName === 'volumenes') {
-      setShowYears(true);
-    } else {
-      setSelectedItem(null);
-      setShowYears(false);
-    }
-    if (itemName === 'Años')
-      if (showYears) {
-        setShowYears(true);
-        setSelectedItem((prevSelectedItem) =>
-          prevSelectedItem === itemName ? null : itemName
-        );
-      }
-  };
-  return (
-    <>
-      <div className={styles.toolbar}>
+import styles from './styles.module.css';
+const Navbar = () => {
+    return (
+        <nav className={styles.nav}>
         <ul>
-          <li>
-            <Link
-              href='/'
-              onClick={() => handleItemClick('inicio')}
-            >
-              INICIO
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/'
-              onClick={() => handleItemClick('articulos')}
-            >
-              ARTÍTCULOS
-            </Link>
-          </li>
-
-          <li className={styles.volumen}>
-            <Link
-              href={`/volumes`}
-              onClick={() => handleItemClick('volumenes')}
-            >
-              VOLÚMENES
-            </Link>
-          </li>
-        </ul>{' '}
-        {showYears && (
-          <button
-            className={styles.fab}
-            onClick={() => handleItemClick('Años')}
-          >
-            AÑOS
-          </button>
-        )}
-        {selectedItem && <Years />}
-      </div>
-    </>
-  );
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/post">Posts</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+        </ul>
+        </nav>
+    );
 };
-
 export default Navbar;
