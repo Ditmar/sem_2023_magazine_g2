131e59d1cd7c2dc777b86c053159254ee96f5fbc
unknown <calemamanispe.12@gmail.com>
2023-12-12 09:51:44 -0400

    features(common):Fix UI URL To View Volume Items[SEM-15]

--- src/app/async/api.ts
+++ src/app/async/api.ts
@@ -1,23 +1,38 @@
-export const fetchData = async (): Promise<Article[]> => {
+type RequestParams = {
+    method: string;
+    headers?: HeadersInit;
+    body?: BodyInit | null;
+  };
+  
+  type FetchOptions = {
+    responseType: 'json' | 'blob';
+  };
+  
+  async function fetchService<T>(
+    url: string,
+    params: RequestParams,
+    options: FetchOptions
+  ): Promise<T> {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        // console.log(data);
-        return data;
+      const response = await fetch(url, params);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      return options.responseType === 'json' ? await response.json() : await response.blob();
     } catch (error) {
-        console.error('Fetch error:', error);
-        return [];
+      console.error('Fetching error:', error);
+      throw error;
     }
-};
-
-interface Article {
-    postId: number;
-    id: number;
-    name: string;
-    email: string;
-    body: string;
-}
+  }
+  
+  async function getJsonData<T>(url: string): Promise<T> {
+    return fetchService<T>(url, { method: 'GET' }, { responseType: 'json' });
+  }
+  
+  async function getFile(url: string): Promise<Blob> {
+    return fetchService<Blob>(url, { method: 'GET' }, { responseType: 'blob' });
+  }
+  
+  export { getJsonData, getFile };
