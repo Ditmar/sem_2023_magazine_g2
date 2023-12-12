2d3965e419fa693f73afe182d528e48d8a95e0b0
jesusdx9 <jesusdx9@gmail.com>
2023-12-11 22:37:40 -0400

    versionnext

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -12,144 +12,92 @@ import dataJson from '../Mocks/data.json'
 
 
 const endpoint = "https://fakestoreapi.com/products"
-
-const Navbar: React.FC = () => {
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [showYears, setShowYears] = useState(false);
-
-  const [toogle, setToogle] = useState(false)
-  const initialFormState = { search: '' };
-  const [form, handlerChangeForm, handlerResetForm] = useForm(initialFormState);
-  // Comentar las lineas 17 y 18 para usar la api
-  const [data, setData] = useState(dataJson)
-  const [result, setResult] = useState([])
-  // Descomentar para usar la api
-
-  // const [data, setData] = useState([])
-  // const [result, setResult] = useState([])
-  const handleClick = () => {
-    setToogle((prevToogle) => !prevToogle)
-  }
-  const handleItemClick = (itemName: string) => {
-    if (itemName === 'volumenes') {
-      setShowYears(true);
-    } else {
-      setSelectedItem(null);
-      setShowYears(false);
+  const Navbar: React.FC = () => {
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [showYears, setShowYears] = useState(false);
+  
+    const [toogle, setToogle] = useState(false)
+    const initialFormState = { search: '' };
+    const [form, handlerChangeForm, handlerResetForm] = useForm(initialFormState);
+    // Comentar las lineas 17 y 18 para usar la api
+    const [data, setData] = useState(dataJson)
+    const [result, setResult] = useState([])
+    // Descomentar para usar la api
+  
+    // const [data, setData] = useState([])
+    // const [result, setResult] = useState([])
+    const handleClick = () => {
+      setToogle((prevToogle) => !prevToogle)
     }
-    if (itemName === 'Años')
-      if (showYears) {
+    const handleItemClick = (itemName: string) => {
+      if (itemName === 'volumenes') {
         setShowYears(true);
-        setSelectedItem((prevSelectedItem) =>
-          prevSelectedItem === itemName ? null : itemName
-        );
+      } else {
+        setSelectedItem(null);
+        setShowYears(false);
       }
-  };
-
-    // Busqueda por titulo
-    const handleSearch = (event: FormEvent) => {
-      event.preventDefault()
-      const filter = data.filter((item) => item.title.toLowerCase().includes(form.search.toLowerCase()))
-      setResult(filter)
-      handlerResetForm()
-    }
-    const fetchData = async (endpoint: any) => {
-  
-      try {
-        const response = await fetch(endpoint, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        });
-  
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      if (itemName === 'Años')
+        if (showYears) {
+          setShowYears(true);
+          setSelectedItem((prevSelectedItem) =>
+            prevSelectedItem === itemName ? null : itemName
+          );
         }
+    };
   
-        return await response.json();
-      } catch (error) {
-        console.error('Error in fetchData:', error);
-        throw error;
+      // Busqueda por titulo
+      const handleSearch = (event: FormEvent) => {
+        event.preventDefault()
+        const filter = data.filter((item) => item.title.toLowerCase().includes(form.search.toLowerCase()))
+        setResult(filter)
+        handlerResetForm()
       }
-    }
-  
-    //Comentar este useEffect para usar mock dataJson
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         const res = await fetchData(endpoint)
-    //         setData(res)
-    //         setResult(res)
-    //     }
-  
-    //     getData()
-  
-    // }, [])
-
-  return (
-    <>
-      <div className={styles.toolbar}>
-        <ul>
-          <li>
-            <Link
-              href='/'
-              onClick={() => handleItemClick('inicio')}
-            >
-              INICIO
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/'
-              onClick={() => handleItemClick('articulos')}
-            >
-              ARTÍTCULOS
-            </Link>
-          </li>
-
-          <li className={styles.volumen}>
-            <Link
-              href={`/volumes`}
-              onClick={() => handleItemClick('volumenes')}
-            >
-              VOLÚMENES
-            </Link>
-          </li>
-        </ul>{' '}
-        <button className={styles.btnSearch} onClick={handleClick}>
-          <img src={SearchIcon.src} alt="searchIcon" />
-        </button>
-        <div className={styles.form}>
-          <form className={`${styles.container} ${toogle ? styles.expanded : ''}`} onSubmit={handleSearch}>
-            <input name='search' value={form.search} onChange={handlerChangeForm} type="text" />
-            <button type='submit' onClick={handleSearch}>buscar</button>
-          </form>
-          {
-            result.length > 0 ? (
-              <div className={styles.sectionCards}>
-                <div className={styles.sectionTitle}>
-                  <span>articulos</span>
-                </div>
-                {
-                  result.map((item, index) => (
-                    <Card key={index} item={item} />
-                  ))
-                }
-              </div>
-            ) : null
+      const fetchData = async (endpoint: any) => {
+    
+        try {
+          const response = await fetch(endpoint, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          });
+    
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
           }
-        </div>
-        {showYears && (
-          <button
-            className={styles.fab}
-            onClick={() => handleItemClick('Años')}
-          >
-            AÑOS
-          </button>
-        )}
-        {selectedItem && <Years />}
-      </div>
-    </>
-  );
+    
+          return await response.json();
+        } catch (error) {
+          console.error('Error in fetchData:', error);
+          throw error;
+        }
+      }
+    
+      //Comentar este useEffect para usar mock dataJson
+      // useEffect(() => {
+      //     const getData = async () => {
+      //         const res = await fetchData(endpoint)
+      //         setData(res)
+      //         setResult(res)
+      //     }
+    
+      //     getData()
+    
+      /
+    return (
+        <nav className={styles.nav}>
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/post">Posts</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+        </ul>
+        </nav>
+    );
 };
 export default Navbar;
