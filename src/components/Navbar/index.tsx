cac43066e04ded5ae1c0b6a2fe225429082cfc38
jesusdx9 <jesusdx9@gmail.com>
2023-12-12 14:54:36 -0400

    feature(common): style fix, version correction, etc

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,117 +1,72 @@
-import Link from 'next/link';
-import styles from './styles.module.css';
-import BurguerButton from './BurguerButton';
-import styled  from 'styled-components'
-function Navbar() {
+'use client';
 
-  const [clicked, setClicked] = useState(false)
-  const handleClick = () => {
-        setClicked(!clicked)
-  }
+import { useState } from 'react';
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import Years from '../../app/years/page';
+
+
+const Navbar: React.FC = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [showYears, setShowYears] = useState(false);
+
+  const handleItemClick = (itemName: string) => {
+    if (itemName === 'volumenes') {
+      setShowYears(true);
+    } else {
+      setSelectedItem(null);
+      setShowYears(false);
+    }
+    if (itemName === 'Años')
+      if (showYears) {
+        setShowYears(true);
+        setSelectedItem((prevSelectedItem) =>
+          prevSelectedItem === itemName ? null : itemName
+        );
+      }
+  };
   return (
     <>
-      <NavContainer>
-        <h2><span>Menu</span></h2>
-        <div className={`links ${clicked ? 'active' : ''}`}>
-          <a onClick={handleClick} href="#h">ARTICULO</a>
-          <a onClick={handleClick} href="#h">INICIO</a>
-          <a onClick={handleClick} href="#h">VOLUMENES</a>
-        </div>
-        <div className='burguer'>
-          <BurguerButton clicked={clicked} handleClick={handleClick} />
-        </div>
-        <BgDiv className={`initial ${clicked ? ' active' : ''}`}></BgDiv>
-      </NavContainer>
+      <div className={styles.toolbar}>
+        <ul>
+          <li>
+            <Link
+              href='/'
+              onClick={() => handleItemClick('inicio')}
+            >
+              INICIO
+            </Link>
+          </li>
+          <li>
+            <Link
+              href='/'
+              onClick={() => handleItemClick('articulos')}
+            >
+              ARTÍTCULOS
+            </Link>
+          </li>
+
+          <li className={styles.volumen}>
+            <Link
+              href={`/volumes`}
+              onClick={() => handleItemClick('volumenes')}
+            >
+              VOLÚMENES
+            </Link>
+          </li>
+        </ul>{' '}
+        {showYears && (
+          <button
+            className={styles.fab}
+            onClick={() => handleItemClick('Años')}
+          >
+            AÑOS
+          </button>
+        )}
+        {selectedItem && <Years />}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
-
-
-const NavContainer = styled.nav`
-  h2{
-    color: white;
-    font-weight: 400;
-    span{
-      font-weight: bold;
-    }
-  }
-  padding: .4rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  a{
-    
-    font: roboto;
-    color: white;
-    text-decoration: none;
-    margin-right: 1rem;
-  }
-  .links{
-    position: absolute;
-    top: -700px;
-    left: -2000px;
-    right: 0;
-    margin-left: auto;
-    margin-right: 10px;
-    text-align: center;
-    transition: all .5s ease;
-    a{
-      color: white;
-      font-size: 2rem;
-      display: block;
-    }
-    @media(min-width: 600px){
-      position: initial;
-      margin: 0;
-      a{
-        font-size: 1rem;
-        color: white;
-        display: inline;
-      }
-      display: block;
-    }
-  }
-  .links.active{
-    width: 100%;
-    display: block;
-    position: absolute;
-    margin-left: auto;
-    margin-right: auto;
-    top: 30%;
-    left: 0;
-    right: 0;
-    text-align: center;
-    a{
-      font-size: 2rem;
-      margin-top: 1rem;
-      color: white;
-    }
-  }
-  .burguer{
-    @media(min-width: 600px){
-      display: none;
-      
-    }
-  }
-`
-
-const BgDiv = styled.div`
-  background-color: #222;
-  position: absolute;
-  top: -1000px;
-  left: -1000px;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-  transition: all .6s ease ;
-  
-  &.active{
-    border-radius: 0 0 80% 0;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`
+export default Navbar;
