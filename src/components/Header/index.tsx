614a53d5574bfb6124d3684f811a2b3bf97984f8
Vecc2424 <sonnyveccmontoyaecos@gmail.com>
2023-12-12 12:55:25 -0400

    Feature(common): The last conflicts were resolved [SEM-3_G2]

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import MainNavigation from '../MainNavigation';
 import styles from './styles.module.scss';
 
@@ -11,17 +10,3 @@ const Header = () => {
 };
 
 export default Header;
-=======
-import MainNavigation from '../MainNavigation/MainNavigation'
-import styles from './styles.module.scss'
-//import ArticulosCientificos from '../ArticulosCientificos/ArticulosCientificos'
-const Header = () => {
-
-    return (
-        <header >
-            header
-        </header>
-    )
-}
-export default Header
->>>>>>> a254854 (feature(common): add viewing articles slider [G2-SEM-3])
